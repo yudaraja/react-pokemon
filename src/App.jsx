960ccf40aff6e91f7b5f838cde55dbce58886fa1
yudaraja <yudaraja43@gmail.com>
2024-10-01 16:29:08 +0700

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
 // App.jsx
-import React from 'react'; // Pastikan Anda mengimpor React
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
-import PokemonList from "./components/PokemonList/PokemonList";
-// Impor komponen lain jika diperlukan
+import React from 'react'; // Pastikan Anda mengimpor React jika menggunakan JSX
+import PokemonList from "./components/PokemonList/PokemonList"; // Pastikan jalur impor benar
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" exact component={PokemonList} />
-        {/* Tambahkan rute lainnya di sini jika ada */}
-      </Switch>
-    </Router>
+    <PokemonList /> // Tidak perlu menggunakan fragment di sini
   );
 }
 
