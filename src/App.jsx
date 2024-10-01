c356595a5348d4146a2ca55e18e231cb762bcd22
yudaraja <yudaraja43@gmail.com>
2024-10-01 16:31:42 +0700

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 // App.jsx
 import React from 'react'; // Pastikan Anda mengimpor React jika menggunakan JSX
 import PokemonList from "./components/PokemonList/PokemonList"; // Pastikan jalur impor benar
+import { HashRouter as Router } from "react-router-dom";
 
 function App() {
   return (
-    <PokemonList /> // Tidak perlu menggunakan fragment di sini
+    <Router>
+      <PokemonList />
+    </Router>
   );
 }
 
